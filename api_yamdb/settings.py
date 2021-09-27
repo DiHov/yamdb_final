970a1480d544a9ac27c5a2ab74a2be42af9531eb
DiHov <78759352+DiHov@users.noreply.github.com>
2021-09-27 08:36:27 +0300

    Update settings.py

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -95,10 +95,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'var/html/static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'var/html/media')
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 
